8e94050c6ff5798aa2f5d5a178bcb68e3f06a696
Ahmad Aljayyousi <153060915+Jayousi7@users.noreply.github.com>
2026-05-13 19:18:44 +0300

    Delete tracker_train directory

--- tracker_train/DownloadData.py
+++
@@ -1,9 +0,0 @@
-import kagglehub
-
-def download_data(path):
-
-    path = kagglehub.dataset_download(path)
-    print("Path to dataset files:", path)
-
-if __name__ == "__main__":
-    download_data(path = r"redzapdos123/modified-aerial-traffic-and-visdrone-dataset-yolo")
